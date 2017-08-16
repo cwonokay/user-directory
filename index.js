667d4e0e02a57bff93cffaa8b358b7c6b4d0af5d
cwonokay <cwonokay@gmail.com>
2017-08-16 08:38:49 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const users    = require('./data.js');
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const app = express();
+
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views")
+app.set("view engine", "mustache")
+//linked "middleware" to html file
+// "/public"add a location for all the files that exsit
+app.use(express.static("public"));
+
+// End Point for home directory
+app.get("/",function(req,res){
+  res.render("index", users);
+
+});
+
+app.get("/index/:id", function(req,res){
+  send
+});
+// Create server and listen on port 3000
+app.listen(3000,function(){
+  console.log("App is ready on port 3000");
+});
